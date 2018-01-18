51aceee8da89441feebd711732451204d2443307
Sean Collings <nightharrier@gmail.com>
2018-01-18 12:23:14 +0200

    changed greeting back

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: "buddy" });
+  res.send({ hi: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
