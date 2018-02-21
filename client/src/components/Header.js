b5d8bdcbcbd81d321d0bdfad27d511f27607ac9e
collingss <nightharrier@gmail.com>
2018-02-21 11:21:56 +0200

    Added responsive styling to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,41 +7,125 @@ import logo from '../logo.svg';
 import './App.css';
 
 class Header extends Component {
+  constructor() {
+    super()
+
+    this.state = { mobileWidth: false };
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    if (window.innerWidth < 520) {
+      this.setState({ mobileWidth: true });
+    } else {
+      this.setState({ mobileWidth: false });
+    }
+  }
+
+  mobileWidth() {
+    return this.state.mobileWidth;
+  }
+
+  renderLogo() {
+    return (
+      <Link
+        to={this.props.auth ? '/surveys' : '/'}
+        className={this.mobileWidth() ? "center brand-logo" : "left brand-logo"}
+      >
+        <img
+          src={logo}
+          className={this.mobileWidth() ? "App-logo-mobile" : "App-logo-hidden"}
+          alt="logo" />
+        Emaily
+        <img src={logo} className="App-logo" alt="logo" />
+      </Link>
+    );
+  }
+
+  renderNav() {
+    if (this.mobileWidth()) {
+      return (
+        <nav className="nav-extended">
+          <div className="nav-wrapper">
+            {this.renderLogo()}
+          </div>
+          {this.renderContent()}
+        </nav>
+      );
+    }
+
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderLogo()}
+          {this.renderContent()}
+        </div>
+      </nav>
+    );
+  }
+
   renderContent() {
     switch (this.props.auth) {
       case null:
         return;
       case false:
         return (
-          <li><a href="/auth/google">Login With Google</a></li>
+          <div className={this.mobileWidth() ? "nav-content" : "right"}>
+            <ul className={this.mobileWidth() ? "tabs tabs-transparent" : ""}>
+              <li
+                className={this.mobileWidth() ? "tab" : ""}>
+                <a href="/auth/google">
+                  Login With Google
+              </a>
+              </li>
+            </ul>
+          </div>
         );
       default:
-        return [
-          <li key="1"><Payments /></li>,
-          <li key="3" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}
-          </li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
-        ];
+        if (this.mobileWidth()) {
+          return (
+            <div className="nav-content">
+              <ul className="tabs tabs-transparent">
+                {this.renderTabs()}
+              </ul>
+            </div>
+          );
+        }
+
+        return (
+          <ul className="right">
+            {this.renderTabs()}
+          </ul>
+        );
     }
+  }
+
+  renderTabs() {
+    return [
+      <li key="1" className={this.mobileWidth() ? "tab" : ""}>
+        <Payments mobileWidth={this.mobileWidth()} />
+      </li>,
+      <li key="3" className={this.mobileWidth() ? "tab" : ""} style={{ margin: '0 10px' }}>
+        Credits: {this.props.auth.credits}
+      </li>,
+      <li key="2" className={this.mobileWidth() ? "tab" : ""}>
+        <a href="/api/logout">Logout</a>
+      </li>
+    ];
   }
 
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? '/surveys' : '/'}
-            className="left brand-logo"
-          >
-            Emaily
-            <img src={logo} className="App-logo" alt="logo" />
-          </Link>
-          <ul className="right">
-            {this.renderContent()}
-          </ul>
-        </div>
-      </nav>
+      <div>{this.renderNav()}</div>
     );
   }
 }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -13,7 +13,7 @@ class Payments extends Component {
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn">
+        <button className={this.props.mobileWidth ? "teal lighten-1 btn-flat white-text" : "btn"}>
           Add Credits
         </button>
       </StripeCheckout>
